a88c3f7d5871f86cbbf0c07d9bfc4f49b3a0bb42
André Canuto <andrecanuto83@gmail.com>
2025-04-13 11:58:16 -0300

    ajuste no app run

--- Produtos.Api/Program.cs
+++ Produtos.Api/Program.cs
@@ -1,5 +1,6 @@
 using Produtos.Api.Controllers;
 using Produtos.Api.Settings;
+using System.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.Ioc();
@@ -11,4 +12,11 @@ app.AddProdutosControllers();
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseHttpsRedirection();
-app.Run(/*"https://0.0.0.0:8081"*/);
+
+//app.UseAuthentication();
+//app.UseAuthorization();
+
+if (Debugger.IsAttached)
+    app.Run();
+else
+    app.Run("http://0.0.0.0:8081");
